10923c85032783f8f07a16d639b52375aec6c9d1
miaozhirui <miaozhirui@ziseyiliao.com>
2016-06-22 01:18:13 +0800

    添加所有的项目

---
+++ ajax/cross_domain/1.php
@@ -0,0 +1,4 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$arr = array('name'=>'miaozhirui');
+echo json_encode($arr);
---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+$array = array("code"=>1,"content"=>"","message"=>"");
+
+echo json_encode($array);
